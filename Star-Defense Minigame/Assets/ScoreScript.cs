b31e6ac8b3b1ce9b017744cfad9b5fe6f1080eb1
Michael Foraker <mikeforaker592001@gmail.com>
2024-01-21 18:31:08 -0500

    First commit

---
+++ Star-Defense Minigame/Assets/ScoreScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    public TMP_Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score: " + scoreValue;
+        if (scoreValue == 100)
+        {
+            LevelManager.manager.YouWin();
+        }
+    }
+}
